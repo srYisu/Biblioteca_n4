2cba4b09d49da73459bcf1474248ea1c06e7143e
srYisu <jesusdelangel.19876@gmail.com>
2024-12-11 02:37:06 -0700

    ya tenemos herencia

--- BibliotecaSegundaEdicion/Prestamos/GestionPrestamos.cs
+++ BibliotecaSegundaEdicion/Prestamos/GestionPrestamos.cs
@@ -6,16 +6,10 @@ using System.Threading.Tasks;
 
 namespace BibliotecaSegundaEdicion
 {
-    internal class GestionPrestamos
+    internal class GestionPrestamos : GestionLibros
     {
         public string usuario { get; set; }
         public int id { get; set; }
-        public string titulo { get; set; }
-        public string autor { get; set; }
-
-        public int ISBN { get; set; }
         public string estado { get; set; }
-
-
     }
 }
